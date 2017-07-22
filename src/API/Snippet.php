900ae9d634e7e4909d90b312c322fd3b8cd1189f
Brian Faust <hello@brianfaust.de>
2017-07-22 07:19:41 +0300

    Refactor to use faustbrian/http

---
+++ src/API/Snippet.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of Snipplr PHP Client.
+ *
+ * (c) Brian Faust <hello@brianfaust.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BrianFaust\Snipplr\API;
+
+use BrianFaust\Snipplr\AbstractAPI;
+
+class Snippet extends AbstractAPI
+{
+    public function listing($tags, $sort = 'title', $limit = null): HttpResponse
+    {
+        return $this->client->post('snippet.list', compact('tags', 'sort', 'limit'));
+    }
+
+    public function details($snippet_id): HttpResponse
+    {
+        return $this->client->post('snippet.get', compact('snippet_id'));
+    }
+
+    public function create($title, $code, $tags = null, $language = null): HttpResponse
+    {
+        return $this->client->post('snippet.post', compact('title', 'code', 'tags', 'language'));
+    }
+
+    public function delete($snippet_id): HttpResponse
+    {
+        return $this->client->post('snippet.delete', compact('snippet_id'));
+    }
+
+    public function validateKey(): HttpResponse
+    {
+        return $this->client->post('user.checkkey');
+    }
+
+    public function languages(): HttpResponse
+    {
+        return $this->client->post('languages.list');
+    }
+}
